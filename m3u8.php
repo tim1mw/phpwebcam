136dc442ab0787e2bad3d42cfd5354691c684b07
Tim Williams <tmw@autotrain.org>
2017-10-02 20:15:19 +0100

    Add code to check that a stream exists before adding it too the master m3u8 file, also pause if it's not found immediatly.

--- m3u8.php
+++ m3u8.php
@@ -14,8 +14,18 @@ try {
     $str = "#EXTM3U\n";
     $streams = $camera->getStreams();
     foreach ($streams as $stream) {
-        $str.= "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=".($stream['bitrate_kbps']*1000)."\n";
-        $str.= "segments/".$camkey."-".$stream['bitrate_kbps']."/streaming.m3u8\n";
+        $file = "segments/".$camkey."-".$stream['bitrate_kbps']."/streaming.m3u8";
+        // Check the file exists, if not wait fir the length of a segment before trying a second time and then skip it if it's still missing
+        if (file_exists($file)) {
+            $str.= "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=".($stream['bitrate_kbps']*1000)."\n";
+            $str.= $file."\n";
+        } else {
+            sleep($CONFIG['segment_time']);
+            if (file_exists($file)) {
+                $str.= "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=".($stream['bitrate_kbps']*1000)."\n";
+                $str.= $file."\n";
+            }
+        }
     }
 
     //header("Expires: "+gmdate('D, d M Y H:i:s T');
